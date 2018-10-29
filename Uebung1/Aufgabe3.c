38e29910716666ccd15fe57f63bc4b8d91c0e96a
FunnyNameHaha <anton.bilchinski@googlemail.com>
2018-10-29 10:28:35 +0100

    Aufgabe 3

---
+++ Uebung1/Aufgabe3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+{
+  printf("------------------------------------------------\n");
+  printf("Teilaufgabe A\n");
+  printf("\"Breaking Bad\"\n\n");
+
+  printf("------------------------------------------------\n");
+  printf("Teilaufgabe B\n");
+  printf("%%\\tt/%%\n\n");
+  printf("------------------------------------------------\n");
+  printf("Teilaufgabe C\n");
+  printf("------------------------------------------------\n");
+  printf("Teilaufgabe D\n");
+  printf("------------------------------------------------\n");
+  printf("Teilaufgabe E\n");
+  printf("------------------------------------------------\n");
+  printf("Teilaufgabe F\n");
+  printf("------------------------------------------------\n");
+  printf("Teilaufgabe G\n");
+
+  return 0;
+}
