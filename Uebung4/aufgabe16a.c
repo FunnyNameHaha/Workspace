a5afdc5111e474cfd509bddfb97421a4df13a9d0
FunnyNameHaha <anton.bilchinski@googlemail.com>
2018-11-20 14:12:48 +0100

    Übung 4 fertig 2.0

--- Uebung4/aufgabe16a.c
+++ Uebung4/aufgabe16a.c
@@ -10,23 +10,34 @@ int main(void)
 {
         char zeichen1;
         char zeichen2;
+        char zeichen3;
+        char zeichen4;
 
         printf("\n~~Dieses Programm ueberprueft ein eingelesenes Zeichen~~\n");
-        printf("Geben Sie nun ein Zeichen ein: ");
-        scanf("%c", &zeichen1);
-
         printf("\n--------------------------------------------------");
         printf("\n0 ist falsch, 1 ist wahr\n");
         printf("--------------------------------------------------\n");
-        printf("Ist das Zeichen ein Grossbuchstabe: %d\n", isupper(zeichen1));
-        printf("Ist das Zeichen ein Buchstabe: %d\n", isalpha(zeichen1));
+        printf("Ist das Zeichen ein Grossbuchstabe \n");
+        printf("Bitte geben Sie nun ein Zeichen ein: " );
+        scanf(" %c", &zeichen1);
+        printf("Ergebnis: %d\n", isupper(zeichen1));
+        printf("--------------------------------------------------\n");
+        printf("Ist das Zeichen ein Buchstabe \n");
+        printf("Bitte geben Sie nun ein Zeichen ein: ");
+        scanf(" %c", &zeichen2);
+        printf("Ergebnis: %d\n", isalpha(zeichen2));
+        printf("--------------------------------------------------\n");
+        printf("Diese Funktion wandelt einen Grossbuchstaben \nzu einem Kleinbuchstaben um \n");
+        printf("Bitte geben Sie nun ein Zeichen ein: ");
+        scanf(" %c", &zeichen3);
 
-        zeichen2 = tolower(zeichen1); /*Extraschritt um zu schauen, ob tolower etwas getan hat*/
+        zeichen4 = zeichen3;
+        zeichen3 = tolower(zeichen3); /*Extraschritt um zu schauen, ob tolower etwas getan hat*/
 
-        if (zeichen1 == zeichen2) {
+        if (zeichen3 == zeichen4) {
                 printf("Tolower hatte keinen Effekt, da das Zeichen kein Grossbuchstabe war\n");
         } else {
-                printf("Tolower hat den Buchstaben %c in %c verwaldelt\n", zeichen1, zeichen2);
+                printf("Tolower hat den Buchstaben %c zu %c verkleinert\n", zeichen4, zeichen3);
         }
 
         return 0;
