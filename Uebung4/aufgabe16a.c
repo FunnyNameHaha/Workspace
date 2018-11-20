45b341756fce8ae783ae8784a9cf5b4704095b4c
FunnyNameHaha <anton.bilchinski@googlemail.com>
2018-11-20 12:30:46 +0100

    Übung 4 fertig

--- Uebung4/aufgabe16a.c
+++ Uebung4/aufgabe16a.c
@@ -8,22 +8,25 @@ int isalpha(int c);
 
 int main(void)
 {
-        char zeichen;
+        char zeichen1;
+        char zeichen2;
 
         printf("\n~~Dieses Programm ueberprueft ein eingelesenes Zeichen~~\n");
         printf("Geben Sie nun ein Zeichen ein: ");
-        scanf("%c", &zeichen);
+        scanf("%c", &zeichen1);
 
         printf("\n--------------------------------------------------");
         printf("\n0 ist falsch, 1 ist wahr\n");
         printf("--------------------------------------------------\n");
-        printf("Ist das Zeichen ein Grossbuchstabe: %d\n", isupper(zeichen));
-        printf("Ist das Zeicher ein Buchstabe: %d\n", isalpha(zeichen));
+        printf("Ist das Zeichen ein Grossbuchstabe: %d\n", isupper(zeichen1));
+        printf("Ist das Zeichen ein Buchstabe: %d\n", isalpha(zeichen1));
 
-        if (tolower(zeichen) == zeichen) {
-                printf("Tolower hatte keinen Effekt\n");
+        zeichen2 = tolower(zeichen1); /*Extraschritt um zu schauen, ob tolower etwas getan hat*/
+
+        if (zeichen1 == zeichen2) {
+                printf("Tolower hatte keinen Effekt, da das Zeichen kein Grossbuchstabe war\n");
         } else {
-                printf("Tolower hat den Buchstaben %c in %c verwaldelt\n", zeichen, tolower(zeichen));
+                printf("Tolower hat den Buchstaben %c in %c verwaldelt\n", zeichen1, zeichen2);
         }
 
         return 0;
@@ -47,7 +50,7 @@ int tolower(int c)
 
 int isalpha(int c)
 {
-        if ((65 <= c && c <= 90) || (97 <= c && c <= 132)) {
+        if ((65 <= c && c <= 90) || (97 <= c && c <= 122)) {
                 return 1;
         }
         return 0;
