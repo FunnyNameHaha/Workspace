07c76657b21a97044c63adbeeb51b780cb277ad0
FunnyNameHaha <anton.bilchinski@googlemail.com>
2018-12-02 13:18:46 +0100

    style changes

--- Blatt5/19c.c
+++
@@ -1,8 +0,0 @@
-
-void smult(int w[], int n, int x)
-{
-        int i;
-        for(i = 0; i<n; i++) {
-                w[i] = w[i] * x;
-        }
-}
--- Blatt5/20a.c
+++
@@ -1,9 +0,0 @@
-void my_strcpy(char s[], const char ct[])
-{
-        int i;
-        for(i = 0; ct[i] != '\0'; i++)
-        {
-                s[i] = ct[i];
-        }
-        s[i] = '\0';
-}
