5ac09fb7fad7d2a4636090c0a978a73b963dec9c
FunnyNameHaha <anton.bilchinski@googlemail.com>
2018-12-25 21:58:26 +0100

    36c

---
+++ 9/36c.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+char *strrchr(char string[], char scan[])
+{
+        char *ptrstring = string;
+        char *ptrscan = scan;
+        char *temp1, *temp2;
+        char *result = NULL;
+
+        while (*(ptrstring++) != '\0') {
+                if(*(ptrstring) == *(ptrscan++)) {
+                        *temp1 = *ptrstring;
+                        *temp2 = *ptrscan;
+                        while (*(temp2++) != '\0') {
+                                if ((*temp1++) != *temp2) {
+                                        temp1 = NULL;
+                                        temp2 = NULL;
+                                        break;
+                                }
+                        }
+                        if (temp1 != NULL) {
+                        *result = *ptrstring;
+                        }
+                }
+        }
+        return result;
+}
+
+int main(void)
+{
+        char input_1[] = "JodelJodelJooodel";
+        char search_1[] = "odel";
+        char *p = strrchr(input_1, search_1);
+
+        if (p != NULL) {
+                printf("Erfolg!\n");
+        } else {
+                printf("Fehler!\n");
+        }
+}
